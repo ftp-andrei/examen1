ddc169fe65f6580311151df90a3f9173dba24983
Andrei <ftp.andreimiu@gmail.com>
2020-06-22 17:13:55 +0200

    Modificacion doc

--- doc.java
+++ doc.java
@@ -5,7 +5,7 @@
  * @author (Andrei Miu) 
  * @version (22/06/2020)
  * @param X: Numero de la linea
- * @return Line: Contenido de la linea X
+ * @return File: Contenido de la linea X
  */
 public interface doc
 {
@@ -13,6 +13,14 @@ public interface doc
    // Devuelve el contenido del archivo tras insertar la linea line en la linea numero x, 
    // el contenido de la linea x, pasa a ser la linea x+1 y asi sucesivamente.
    public abstract String escribirLinea(int x, String line, String file);
-   
+   /**
+    * @resumen:Devuelve el perimetro de una ciudad en km2
+    * @entrada: String ciudad
+    * @salida: int km
+    * @precondicion: No pueden haber numeros en el string, de ser asi daría error. La ciudad debe ser correcta.
+    * @postcondicion: Devolver el perimetro de una ciudad en km2.
+    */
+   //Devuelve el perimetro de una ciudad en km2
+   public abstract int perimetro (String ciudad, int km);
    
 }
