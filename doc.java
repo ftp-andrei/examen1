f5f2d57e000ebaa3dfd0db34ddf5d879d1145412
Andrei <ftp.andreimiu@gmail.com>
2020-06-22 16:52:06 +0200

    Documentación Doc

--- doc.java
+++ doc.java
@@ -2,13 +2,16 @@
 /**
  * Write a description of class doc here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Andrei Miu) 
+ * @version (22/06/2020)
+ * @param X: Numero de la linea
+ * @return Line: Contenido de la linea X
  */
 public interface doc
 {
    
-   // Devuelve el contenido del archivo tras insertar la linea line en la linea numero x, el contenido de la linea x, pasa a ser la linea x+1 y asi sucesivamente.
+   // Devuelve el contenido del archivo tras insertar la linea line en la linea numero x, 
+   // el contenido de la linea x, pasa a ser la linea x+1 y asi sucesivamente.
    public abstract String escribirLinea(int x, String line, String file);
    
    
